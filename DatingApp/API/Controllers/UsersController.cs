87e30ebb5112d69ae688d5a76348fec652287a5f
Your Name <you@example.com>
2021-05-11 00:35:57 +0530

    End of section 10 update, State mngmt in angular

--- DatingApp/API/Controllers/UsersController.cs
+++ DatingApp/API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Controllers;
 using API.Data;
@@ -36,8 +37,18 @@ namespace DatingApp.API.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
-            
+        }
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
 
+            _mapper.Map(memberUpdateDto, user);
+            _userRepository.update(user);
+            
+            if (await _userRepository.SaveAllAsync()) { return NoContent(); }
+            return BadRequest("Faild to update user");
         }
     }
 }
